f1579609348b522ee790f8311d76946adeccf2ea
bbm <brian.maranville@nist.gov>
2025-07-14 11:19:27 -0400

    rename first arg to h5wasm_module for clarity

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 // To use these functions, first import h5wasm
 // then get the module with:
 // import h5wasm from 'h5wasm'; // or 'h5wasm/node' for nodejs
-// const h5wasm_module = await h5wasm.ready();
+// const h5wasm_module = await h5wasm.ready;
 //
 // import plugin_helpers from 'h5wasm-plugins';
 // plugin_helpers.list_plugins(h5wasm_module);
@@ -21,28 +21,28 @@ export const plugin_names = [
     "zstd",
 ]
 
-export async function install_plugins(h5wasm, names=plugin_names, new_plugin_path=null) {
+export async function install_plugins(h5wasm_module, names=plugin_names, new_plugin_path=null) {
     if (new_plugin_path !== null) {
-        h5wasm.insert_plugin_search_path(new_plugin_path, 0);
+        h5wasm_module.insert_plugin_search_path(new_plugin_path, 0);
     }
-    const plugin_path = h5wasm.get_plugin_search_paths()[0];
-    h5wasm.FS.mkdirTree(plugin_path);
+    const plugin_path = h5wasm_module.get_plugin_search_paths()[0];
+    h5wasm_module.FS.mkdirTree(plugin_path);
 
 
     for (const plugin_name of names) {
         const plugin_filename = `libH5Z${plugin_name}.so`;
         const r = await fetch(`${base_url}/plugins/${plugin_filename}`);
         const buf = await r.arrayBuffer();
-        h5wasm.FS.writeFile(`${plugin_path}/${plugin_filename}`, new Uint8Array(buf));
+        h5wasm_module.FS.writeFile(`${plugin_path}/${plugin_filename}`, new Uint8Array(buf));
     }
 }
 
-export function list_plugins(h5wasm) {
-    const plugin_path = h5wasm.get_plugin_search_paths()[0];
-    return h5wasm.FS.readdir(plugin_path);
+export function list_plugins(h5wasm_module) {
+    const plugin_path = h5wasm_module.get_plugin_search_paths()[0];
+    return h5wasm_module.FS.readdir(plugin_path);
 }
 
-export function install_local_plugins(h5wasm) {
+export function install_local_plugins(h5wasm_module) {
     const local_path = base_url.replace(/^file:\/\//, '');
-    h5wasm.insert_plugin_search_path(`${local_path}/plugins`, 0);
+    h5wasm_module.insert_plugin_search_path(`${local_path}/plugins`, 0);
 }
