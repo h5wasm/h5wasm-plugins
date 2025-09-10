6f4a1536156fda103aab47a2a0c44f64ce6e1a99
bbm <brian.maranville@nist.gov>
2025-09-10 11:20:56 -0400

    update test files for JPEG plugin (hcidecomp)

--- test/make_testfile.py
+++ test/make_testfile.py
@@ -18,9 +18,13 @@ for filt in hdf5plugin.get_filters():
         continue
 
     with h5py.File(destdir / f"test_{filter_name}.h5", 'w') as root:
-        root.create_dataset('data', data=list(range(10000)), dtype='<f4', **filt())
-        root['data'].attrs['filter'] = filter_name
-
+        # need to use integer dtype with 1 or 2 bytes for fcidecomp
+        dtype = '<f4' if filter_name != 'fcidecomp' else '<i2'
+        try:
+            root.create_dataset('data', data=list(range(10000)), shape=(100,100), dtype=dtype, **filt())
+            root['data'].attrs['filter'] = filter_name
+        except Exception as e:
+            print(f"Error creating dataset for filter {filter_name}: {e}")
 
 with h5py.File("test_filters.h5", "w") as root:
     root.create_dataset('gzip', data=list(range(20000)), dtype='<f4', compression='gzip', compression_opts=4)
@@ -28,5 +32,6 @@ with h5py.File("test_filters.h5", "w") as root:
     for filt in hdf5plugin.get_filters():
         if filt.filter_name == 'sz':
             continue
-        root.create_dataset(filt.filter_name, data=list(range(20000)), dtype='<f4', **filt())
+        dtype = '<f4' if filt.filter_name != 'fcidecomp' else '<i2'
+        root.create_dataset(filt.filter_name, data=list(range(20000)), shape=(100,200), dtype=dtype, **filt())
         root.flush()
